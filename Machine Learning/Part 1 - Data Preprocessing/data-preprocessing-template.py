e12230472dd9f30aa06ec3a411a86d8f59fe19d1
HarshaKy <harshaky6573@gmail.com>
2019-06-19 01:01:57 +0530

    categorical

--- Machine Learning/Part 1 - Data Preprocessing/data-preprocessing-template.py
+++ Machine Learning/Part 1 - Data Preprocessing/data-preprocessing-template.py
@@ -17,6 +17,12 @@ imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encode categorical data
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelEncoder_X = LabelEncoder()
 X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+
+oneHotEncoder = OneHotEncoder(categorical_features=[0])
+X = oneHotEncoder.fit_transform(X).toarray()
+
+labelEncoder_y = LabelEncoder()
+y = labelEncoder_y.fit_transform(y)
